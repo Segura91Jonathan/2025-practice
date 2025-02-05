28c571152b225f1c2888b9d0355cc076ba3f97f6
Segura91Jonathan <j.f.segura91@gmail.com>
2025-02-05 00:20:29 -0300

    250205 first commit

---
+++ JS/2.7.js
@@ -0,0 +1,42 @@
+//2.7 TYPE CONVERSION
+
+
+alert("2.7 TYPE CONVERSION");
+
+//string conversion
+
+alert("string conversion")
+
+let variable = true;
+alert(`the type of variable is ${typeof variable}`);
+
+
+variable = String(variable);
+alert(`the type of variable is ${typeof variable}`);
+
+
+//numeric conversion
+
+alert("numeric conversion");
+
+let numericConversion = "456";
+alert(`the type of stringconversion is ${typeof numericConversion}`);
+
+numericConversion = Number(numericConversion);
+alert(`the type of stringconversion is ${typeof numericConversion}`);
+
+//boolean conversion
+
+alert(`te boolean value of 1  is ${Boolean(1)} `);
+alert(`te boolean value of 0 is ${Boolean(0)} `);
+alert(`te boolean value of "  " is ${Boolean(" ")} `);
+alert(`te boolean value of "a string" is ${Boolean("a string")} `);
+alert(`te boolean value of null is ${Boolean(null)} `);
+alert(`te boolean value of NaN is ${Boolean(NaN)} `);
+
+/*
+
+
+
+
+*/
